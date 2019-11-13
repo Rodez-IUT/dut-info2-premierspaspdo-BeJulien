73bcab3c12d1cd6570d71345b705b7c64b541803
BeJulien <julien.benac@iut-rodez.fr>
2019-11-13 10:11:02 +0100

    fix #2 Récupération et affichage des utilisateurs avec énoncé non préparé

--- all_users.php
+++ all_users.php
@@ -3,35 +3,40 @@
 	<head>
 		<title>All users</title>
 		<meta charset="utf-8 /">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	</head>
 	<body>
-		<?php 
+		<?php
 			$host = 'localhost';
-			$db = 'my-activities';
+			$db   = 'my_activities';
 			$user = 'root';
 			$pass = 'root';
 			$charset = 'utf8mb4';
-
 			$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 			$options = [
-				PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-				PDO::ATTR_EMULATE_PREPARES   => false
+				PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+				PDO::ATTR_EMULATE_PREPARES   => false,
 			];
-
 			try {
-				$pdo = new PDO($dsn, $user, $pass, $options);
-			} catch (PDOException $e) {
-				throw new PDOException ($e->getMessage(), (int) $e->getCode());
+				 $pdo = new PDO($dsn, $user, $pass, $options);
+			} catch (PDOException $e) {
+				 throw new PDOException($e->getMessage(), (int)$e->getCode());
 			}
 		?>
 		
+		<table class="table">
 		<?php
-			$stmt = $pdo->query('SELECT * FROM users');
-
+			$stmt = $pdo->query('SELECT users.id, username, email, name FROM users JOIN status ON status.id = users.status_id ORDER BY username');
 			while ($row = $stmt->fetch()) {
-				echo $row['email'] . '\n';
+				echo '<tr>';
+				echo '<td>' . $row['id'] . '</td>';
+				echo '<td>' . $row['username'] . '</td>';
+				echo '<td>' . $row['email'] . '</td>';
+				echo '<td>' . $row['name'] . '</td>';
+				echo '</tr>';
 			}
 		?>
+		</table>
 	</body>
 </html>
